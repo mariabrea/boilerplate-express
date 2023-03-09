1cc52c8b7ce00741a593a58d5ee8907eb2eefdd1
mariabrea <mariabrea75@hotmail.com>
2023-03-09 15:30:09 -0600

    Add /:word/echo

--- myApp.js
+++ myApp.js
@@ -28,8 +28,11 @@ app.get('/now', function(req, res, next) {
     next();
   }, function(req, res) {
     res.json({time: req.time});
-  });
+});
 
+app.get('/:word/echo', function(req, res) {
+    res.json({echo: req.params.word});
+});
 
 
 
