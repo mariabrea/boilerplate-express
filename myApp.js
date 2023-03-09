84f5c8a0c0567fc8fa15c72177a1729ef27bf11c
mariabrea <mariabrea75@hotmail.com>
2023-03-09 14:20:04 -0600

    Add /json

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 
-app.use(express.static(__dirname));
+app.use("/public", express.static(__dirname + "/public"));
 
 console.log("Hello World");
 
@@ -10,6 +10,9 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", function(req, res){
+    res.json({"message": "Hello json"});
+});
 
 
 
