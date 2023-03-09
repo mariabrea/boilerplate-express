fb60988d5f8535b9d60194c6d733705c4f29f2bd
mariabrea <mariabrea75@hotmail.com>
2023-03-09 15:52:26 -0600

    Add /name

--- myApp.js
+++ myApp.js
@@ -34,6 +34,16 @@ app.get('/:word/echo', function(req, res) {
     res.json({echo: req.params.word});
 });
 
+// app.get('/name', function(req, res) {
+//     res.json({name: req.query.firstname + " " + req.query.lastname});
+// });
+
+app.route("/name").get(function(req, res) {
+    res.json({name: req.query.first + " " + req.query.last});
+}).post(function(req, res) {
+    res.json({name: req.query.first + " " + req.query.last});
+});
+
 
 
 
