f49ea8eafc5656c57bd34c26550868b7d2957c4c
mariabrea <mariabrea75@hotmail.com>
2023-03-10 14:54:50 -0600

    Use rq.body in post requests

--- myApp.js
+++ myApp.js
@@ -43,7 +43,7 @@ app.get('/:word/echo', function(req, res) {
 app.route("/name").get(function(req, res) {
     res.json({name: req.query.first + " " + req.query.last});
 }).post(function(req, res) {
-    res.json({name: req.query.first + " " + req.query.last});
+    res.json({name: req.body.first + " " + req.body.last});
 });
 
 
