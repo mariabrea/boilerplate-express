54293dd815f307aca353de7537373dfca664e099
mariabrea <mariabrea75@hotmail.com>
2023-03-10 14:37:53 -0600

    Add body-parser

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 let express = require('express');
+const bodyParser = require("body-parser");
 let app = express();
 
 
 app.use("/public", express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(function(req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
